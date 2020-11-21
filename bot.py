6d2b44a0e4c4a01767cc020484a46c1360768ca3
Shuana <61404361+shuanaongithub@users.noreply.github.com>
2020-11-21 14:53:14 +0000

    custom prefixes

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import textwrap
 import json
+import discord
 
 from os import environ, listdir
 
@@ -14,19 +15,68 @@ from discord import (
 )
 from discord.ext import commands
 from dotenv import load_dotenv
+from discord.ext.commands import has_permissions
 
 load_dotenv()
 
 # -------
+# prefix area
+
+defaultprefixes = ["World", "w/", "world "]
+
+async def get_prefix(world, message):
+	if await world.is_owner(message.author):
+		return commands.when_mentioned_or("")(world, message)
+	with open('prefixes.json', 'r') as f:
+		prefixes = json.load(f)
+		if not str(message.guild.id) in prefixes:
+			return commands.when_mentioned_or(*defaultprefixes)(world, message)
+	return commands.when_mentioned_or(prefixes[str(message.guild.id)])(world, message)
+
+# -------
 # Main bot area
+
 world = commands.Bot(
-    command_prefix=("w/", "world ", "World "),
+    command_prefix=(get_prefix),
     description="Discord Bot Made For All",
     case_insensitive=True,
     intents=Intents.all()
 )
 world.remove_command("help")
 
+# -------
+
+
+# -------
+# Command area 
+
+@world.command()
+@has_permissions(administrator=True)
+async def changeprefix(ctx, prefix):
+	if len(prefix) >7:
+		return await ctx.send(f"Sorry {ctx.author.mention} a limit of `7` letters please.")
+	with open('prefixes.json', 'r') as f:
+		prefixes = json.load(f)
+
+	prefixes[str(ctx.guild.id)] = prefix
+
+	with open('prefixes.json', 'w') as f:
+		json.dump(prefixes, f, indent=4)
+
+	embed = Embed(
+		title="Custom prefix",
+		description=f"Prefix for {ctx.guild} is now `{prefix}`.\nTry using the help command: `{prefix}help`",
+		color=0x2F3136
+		)
+	await ctx.send(embed=embed)
+
+
+@changeprefix.error
+async def changeprefix_error(ctx, error):
+   if isinstance(error, discord.ext.commands.errors.CheckFailure):
+       await ctx.send(f"Sorry {ctx.author.mention} you don't have permissions to change the prefix!")
+
+# -------
 
 # -------
 # Cogs area
@@ -51,18 +101,16 @@ with open("blacklisted.json") as f:
 async def on_message(message: Message) -> None:
     """
     Dispatched every time a message is sent.
-
     Checks if the message wasn't send by a bot, the message wasn't send
     on DMs and the user isn't blacklisted.
     """
     if message.author.bot:
         return
     if not message.guild:
-        await message.channel.send((
+        return await message.channel.send((
             "You can't use commands on DMs, invite the bot your server: "
             f"<https://discord.com/oauth2/authorize?client_id={world.user.id}&permissions=8&scope=bot>"
         ))
-        return
 
     if message.author.id in blacklisted_people:
         return
@@ -71,7 +119,7 @@ async def on_message(message: Message) -> None:
 
 
 # -------
-# On ready area
+# Event area
 @world.event
 async def on_ready() -> None:
     """Dispatched when the bot has successfully connected into Discord."""
@@ -84,41 +132,13 @@ async def on_ready() -> None:
     )
     print("Bot connected successfully into Discord!")
 
-
-# -------
-# Error handler area
 @world.event
-async def on_command_error(ctx: commands.Context, error: commands.errors.CommandInvokeError) -> None:
-    """Dispatched when an exception was raised."""
-    error = getattr(error, "original", error)
-    if environ["DEBUG"] == "1":
-        error_msg = textwrap.dedent(f"""
-            ------------------
-            Error
-            
-            Cog: {ctx.cog.__class__.__name__}
-            Command: {ctx.command}
-            Invoker: {ctx.author} ({ctx.author.id})
-            Location: {ctx.message.jump_url}
-            Input: `{ctx.message.content}`
-            
-            {error.__class__.__name__}: {error}
-            ------------------
-        """)
-        print(error_msg)
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    prefixes.pop(str(guild.id))
 
-        error_embed = Embed(
-            title=":x: Error",
-            description=error_msg.strip().strip("-"),
-            color=0xff0000
-        )
-        try:
-            error_channel = await world.fetch_channel(763111707379761162)
-        except HTTPException:
-            return
-        await error_channel.send(embed=error_embed)
-    else:
-        print(f"{error.__class__.__name__}: {error}")
-
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
 
 world.run(environ["TOKEN"], bot=True, reconnect=True)
