d8210a8d253defcd187beb82f56496f052b978b3
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-01-11 13:15:37 +0700

    shuana is qt

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,7 +1,7 @@
-import discord
+from discord import Embed
 from asyncio import sleep as _sleep
 from time import time
-from discord.ext import commands
+from ext import commands
 
 class PingCog(commands.Cog):
     def __init__(self, bot):
@@ -11,12 +11,11 @@ class PingCog(commands.Cog):
     @commands.command(help="Show bots latency.")
     @commands.cooldown(rate=1, per=5, type=commands.BucketType.member)
     async def ping(self, ctx):
-        testing = discord.Embed(title = 'Testing Latency... <a:loading1:772860591190048768>', color=self.color)
         start = time()
-        msg = await ctx.send(embed=testing)
+        msg = await ctx.send(embed=Embed(title = 'Testing Latency... <a:loading1:772860591190048768>', color=self.color))
         end = time()
         await _sleep(2)
-        await msg.edit(embed=discord.Embed(title = f'<a:loading:772860569127878676> Pong!', description=f"Latency: `{round(self.bot.latency * 1000)}ms`\nResponse time: `{(end-start)*1000:,.0f}ms`", color=self.color))
+        await msg.edit(embed=Embed(title = f'<a:loading:772860569127878676> Pong!', description=f"Latency: `{round(self.bot.latency * 1000)}ms`\nResponse time: `{(end-start)*1000:,.0f}ms`", color=self.color))
 
     @ping.error
     async def ping_error(self, ctx, error):
