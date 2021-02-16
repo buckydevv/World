2f3c54be6b00e3ffbe62e89a3e8ca8a57a25f2f2
Shuana <61404361+shuanaongithub@users.noreply.github.com>
2021-02-16 22:59:01 +0000

    UNMATCHED )

--- cogs/ping.py
+++ cogs/ping.py
@@ -9,7 +9,7 @@ class PingCog(commands.Cog):
     @commands.command(help="Show bots latency.")
     @commands.cooldown(rate=1, per=5, type=commands.BucketType.member)
     async def ping(self, ctx):
-        return await ctx.send(title = f'<a:loading:772860569127878676> Pong!', description=f"Latency: `{round(self.bot.latency * 1000)}ms`\nResponse time: `{(time() - ctx.message.created_at.timestamp())*1000:,.0f}ms`", color=self.bot.color))
+        return await ctx.send(title = f'<a:loading:772860569127878676> Pong!', description=f"Latency: `{round(self.bot.latency * 1000)}ms`\nResponse time: `{(time() - ctx.message.created_at.timestamp())*1000:,.0f}ms`", color=self.bot.color)
 
     @ping.error
     async def ping_error(self, ctx, error):
