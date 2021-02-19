39ee4bec2130f253990c7929634da8f21b33b9ef
Shuana <61404361+shuanaongithub@users.noreply.github.com>
2021-02-19 22:24:08 +0000

    i think im japanese?  笑 😂

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,4 +1,5 @@
 from discord import Embed
+from asyncio import sleep as _sleep
 from time import time
 from discord.ext import commands
 
@@ -9,7 +10,11 @@ class PingCog(commands.Cog):
     @commands.command(help="Show bots latency.")
     @commands.cooldown(rate=1, per=5, type=commands.BucketType.member)
     async def ping(self, ctx):
-        return await ctx.send(title = f'<a:loading:772860569127878676> Pong!', description=f"Latency: `{round(self.bot.latency * 1000)}ms`\nResponse time: `{(time() - ctx.message.created_at.timestamp())*1000:,.0f}ms`", color=self.bot.color)
+        start = time()
+        msg = await ctx.send(embed=Embed(title = 'Testing Latency... <a:loading1:772860591190048768>', color=self.bot.color))
+        end = time()
+        await _sleep(2)
+        await msg.edit(embed=Embed(title = f'<a:loading:772860569127878676> Pong!', description=f"Latency: `{round(self.bot.latency * 1000)}ms`\nResponse time: `{(end-start)*1000:,.0f}ms`", color=self.bot.color))
 
     @ping.error
     async def ping_error(self, ctx, error):
